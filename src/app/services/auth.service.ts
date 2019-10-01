a86eaa64607e4518e8e1602ba169b6dc65742642
bidachen <bidachen@users.noreply.github.com>
2019-10-01 10:30:28 -0400

    added register and auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { Router } from  "@angular/router";
+import { AngularFireAuth } from  "@angular/fire/auth";
+import { User } from  'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: User;
+  constructor(public angularFireAuth: AngularFireAuth, public router: Router) {
+    this.angularFireAuth.authState.subscribe(user => {
+      if(user){
+        this.user = user;
+        localStorage.setItem('user', JSON.stringify(this.user));
+      }
+      else{
+        localStorage.setItem('user', null);
+      }
+    })
+  }
 
-  constructor() { }
+  async login(email: string, password: string){
+    try{
+      await this.angularFireAuth.auth.signInWithEmailAndPassword(email, password)
+      this.router.navigate(['portfolio']);
+    }
+    catch (e) {
+      alert("error " + e.message);
+    }
+  }
+
+  async logout(){
+    await this.angularFireAuth.auth.signOut();
+    localStorage.removeItem('user');
+    this.router.navigate(['login']);
+  }
 }
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map} from 'rxjs/operators';
+import { AngularFireAuth } from  "@angular/fire/auth";
 @Injectable({
   providedIn: 'root'
 })
-export class RegisterService {
 
-  constructor() { }
+export class RegisterService {
+  constructor(public angularFireAuth: AngularFireAuth,private db: AngularFirestore){}
+  async createUser(newUser){
+    try{
+      await this.angularFireAuth.auth.createUserWithEmailAndPassword(
+      newUser.value.email, newUser.value.password
+    )
+      this.saveUser(newUser);
+      return;
+    }
+    catch(e) {
+      alert(e.message);
+    }
+
+  }
+  
+  saveUser(newUser){
+    this.db.collection('users').doc(newUser.value.email).set({
+      name: formGroup.value.name,
+    }).then(res => {return;})
+    .catch(e => {console.log(e)});
+    this.db.collection('users/' + newUser.value.email + '/transactions').add({});
+}
+
 }
