076f7969701537e2412c053f8c2f6a52d7446042
bidachen <bidachen@users.noreply.github.com>
2019-10-01 15:29:21 -0400

    added and displayed dummy data in portfolio page

--- src/app/components/portfolio-page/portfolio-page.component.ts
+++ src/app/components/portfolio-page/portfolio-page.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Transaction {
+  type: string;
+  stock: string;
+  numOfShares: string;
+  price: number;
+}
 @Component({
   selector: 'app-portfolio-page',
   templateUrl: './portfolio-page.component.html',
   styleUrls: ['./portfolio-page.component.scss']
 })
 export class PortfolioPageComponent implements OnInit {
+  public transactions: Transaction[] = [
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+    {type: 'BUY', stock: '(AAPL)', numOfShares: '6 shares', price: 200},
+  ];
 
+  public displayedColumns = ['type', 'stock', 'numOfShares', 'price'];
   constructor() { }
 
   ngOnInit() {
